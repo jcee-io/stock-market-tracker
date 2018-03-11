b3b22c1d2aa5ed29bfcf0063a9295fb62667bbfa
Justin <jvcruz@ucdavis.edu>
2018-03-10 20:22:45 -0800

    socket.io declared

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ const width = 1250;
 const height = 750;
 const stockList = document.getElementById('stock-list');
 
+const socket = io.connect(location.host);
 const handleSubmit = event => {
 	console.log(event.target.stock.value);
 	event.preventDefault();
