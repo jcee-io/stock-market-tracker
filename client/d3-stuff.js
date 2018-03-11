277c0da6ef2e56cc5f9a051199276174742af97c
Justin <jvcruz@ucdavis.edu>
2018-03-11 04:48:51 -0700

    x-axis format modularized

--- client/d3-stuff.js
+++ client/d3-stuff.js
@@ -9,6 +9,25 @@ const svg = d3.select('svg')
   .attr('height', height);
 
 
+
+const formatXAxis = (timeExtent, timeFormat) => {
+	svg.selectAll('.x-axis')
+	  .remove();
+
+	const timeScale = d3.scaleTime()
+	  .domain(timeExtent)
+	  .range([padding, width - padding]);
+
+	const xAxis = d3.axisBottom(timeScale)
+	  .tickFormat(timeFormat);
+
+	svg.append('g')
+		.classed('x-axis', true)
+	  .attr('transform', `translate(0, ${height - padding})`)
+	  .call(xAxis);
+};
+
+
 const getDate = event => {
 	const now = new Date();
 	const timeLength = event ? event.target.textContent.split(' ') : ['1', 'M'];
@@ -20,32 +39,24 @@ const getDate = event => {
 	const thisDate = now.getDate();
 	const currentDate = new Date(`${thisMonth} ${thisDate} ${thisYear}`);
 	let olderDate;
+	let timeFormat = d3.timeFormat('%b %Y');
 
 	if(timeFactor === 'M') {
 		const tempMonth = thisMonth - timeValue;
 		const selectedMonth = tempMonth <= 0 ? 12 + tempMonth : tempMonth;
+		const selectedYear = tempMonth <= 0 ? thisYear - 1 : thisYear;
+		if(timeValue === 1) {
+			timeFormat = d3.timeFormat('%b %d')
+		}
 
-		olderDate = new Date(`${selectedMonth} ${thisDate} ${thisYear}`);
+		olderDate = new Date(`${selectedMonth} ${thisDate} ${selectedYear}`);
 	} else {
 		const selectedYear = thisYear - timeValue;
 		olderDate = new Date(`${thisMonth} ${thisDate} ${selectedYear}`);
 	};
 
-	svg.selectAll('.x-axis')
-	  .remove();
 
-	const timeScale = d3.scaleTime()
-	  .domain([olderDate, currentDate])
-	  .range([padding, width - padding]);
-
-	const xAxis = d3.axisBottom(timeScale)
-	  .tickFormat(d3.timeFormat('%b %d'));
-
-	svg.append('g')
-		.classed('x-axis', true)
-	  .attr('transform', `translate(0, ${height - padding})`)
-	  .call(xAxis);
-
+	formatXAxis([olderDate, currentDate], timeFormat);
 };
 
 getDate();
