7566e5b50096a4c58931b812c3e4415a6a00ae16
Justin <jvcruz@ucdavis.edu>
2018-03-13 19:11:10 -0700

    stock schema created

---
+++ database/schema.js
@@ -0,0 +1,20 @@
+module.exports = async connection => {
+  try {
+   await connection.connect(); 
+  } catch (err) {
+    console.log(err);
+  }
+	
+	
+	await connection.queryAsync('CREATE DATABASE IF NOT EXISTS Stocks');
+	await connection.queryAsync('USE Stocks');
+	
+
+	await connection.queryAsync(
+		`CREATE TABLE IF NOT EXISTS Stocks (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(255) NOT NULL
+		)`
+	);
+
+};
